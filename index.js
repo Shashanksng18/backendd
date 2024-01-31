d125f1ba05794a4edc48fe0525a52c899d246ca0
Shashank Singh Bhandari <Shashanksng20@gmail.com>
2024-01-31 13:36:03 +0530

    adding employee

--- index.js
+++ index.js
@@ -4,12 +4,31 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+
+const employee = [
+    {
+        name: "emp1"
+    },
+    {
+        name: "emp2"
+    },
+    {
+        name: "emp3"
+    },
+    {
+        name: "emp3"
+    }
+]
 app.get("/", (req, res) => {
     res.json("i am get api")
 })
 
 app.get("/info", (req, res) => {
     res.json("info file")
+})
+
+app.get("/emp", (req, res) => {
+    res.status(200).json(employee)
 })
 
 const port = process.env.PORT || 5000;
